68536c45255853761f25565809c3f131edaff117
jayascript1 <jojlondon@gmail.com>
2023-04-28 14:06:42 +0100

    mapping out profile

--- src/components/Profile components/Profile.jsx
+++ src/components/Profile components/Profile.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import '../../css/Profile.css'
 
 export const Profile = () => {
   return (
-    <div className='profile'>Profile</div>
+    <div className='profile'>
+      <header>
+        <img className='profile-picture' src='./userIcon.png' alt  />
+      </header>
+      <div className='profile-container'>
+        <p> Email: </p>
+        <p> Change Password </p>
+        <p> View completed tasks </p>
+        <p> Log out </p>
+
+      </div>
+    </div>
   )
 }
