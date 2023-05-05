4180483f868be0a66739430c0d5a47439fc2323e
RoKen1000 <rosskennedy993@outlook.com>
2023-05-05 11:30:13 +0100

    Create functionality to allow user to change their password and change their profile picture.

--- src/components/Profile components/Profile.jsx
+++ src/components/Profile components/Profile.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "../../css/Profile.css";
-import { updatePassword, onAuthStateChanged } from "firebase/auth";
+import { updatePassword, onAuthStateChanged, updateProfile, EmailAuthProvider, reauthenticateWithCredential} from "firebase/auth";
 import { auth } from "../../firebase/firebase";
 import { storage } from "../../firebase/firebase";
-import { ref, uploadBytesResumable } from "firebase/storage";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import CompletedTasks from "./CompletedTasks";
 
 export const Profile = () => {
@@ -12,6 +12,7 @@ export const Profile = () => {
   const [showChangePasswordButton, setShowChangePasswordButton] =
     useState(true);
 
+  const [oldPassword, setOldPassword] = useState("")
   const [newPassword, setNewPassword] = useState("");
   const [confirmNewPassword, setConfirmNewPassword] = useState("");
   const [successfulPasswordChange, setSuccessfulPasswordChange] =
@@ -20,27 +21,39 @@ export const Profile = () => {
   const [showChangeImageForm, setShowChangeImageForm] = useState(false);
   const [showCompletedTaskButton, setShowCompletedTaskButton] = useState(true);
   const [renderCompletedTask, setRenderCompletedTask] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
+  const [profilePicture, setProfilePicture] = useState("")
 
+  
   useEffect(() => {
-    const getUsersEmail = () => {
+    const getUserDetails = () => {
       onAuthStateChanged(auth, (userAuth) => {
         if (userAuth) {
           setEmail(userAuth.email);
+          setProfilePicture(userAuth.photoURL)
         }
       });
     };
-    getUsersEmail();
+    getUserDetails()
+
   }, []);
 
-  const imageRef = ref(storage, "images/" + File.name);
-
-  const handleImageSubmit = () => {
-    uploadBytesResumable(imageRef, File).then(() => {
-      console.log("upload Successful");
-    });
+  const handleImageSubmit = (e) => {
+    e.preventDefault();
+    const imageRef = ref(storage, "images/" + e.target[0].files[0].name);
+    uploadBytesResumable(imageRef, e.target[0].files[0])
+    .then((image) => {
+      return getDownloadURL(ref(storage, image.ref.fullPath))
+    })
+    .then((URL) => {
+      return updateProfile(auth.currentUser, {photoURL: URL})
+    })
+    .then(() => {
+      setProfilePicture(auth.currentUser.photoURL)
+      console.log("Profile Picture change successful")
+    })
   };
 
   const changePasswordButtonClickHandler = () => {
@@ -70,8 +83,12 @@ export const Profile = () => {
       return 
     }
 
-
-    updatePassword(user, newPassword).then(() => {
+    const credential = EmailAuthProvider.credential(user.email, oldPassword)
+    reauthenticateWithCredential(user, credential)
+    .then(() => {
+      updatePassword(user, newPassword)
+    })
+    .then(() => {
       setSuccessfulPasswordChange(true);
     })
     .catch((error) => {
@@ -79,12 +96,14 @@ export const Profile = () => {
     })
   };
 
+  if(isLoading) return <p>Loading...</p>
+
   return (
     <div className="profile">
       <header>
         <img
           className="profile-picture"
-          src="./userIcon.png"
+          src={profilePicture ? profilePicture: "../../public/userIcon.png"}
           alt="profile-picture"
         />
       </header>
@@ -99,7 +118,7 @@ export const Profile = () => {
           </button>
         )}
         {showChangeImageForm && (
-          <form onSubmit={(e) => handleImageSubmit(e)}>
+          <form method="post" encType="multipart/form-data" onSubmit={(e) => handleImageSubmit(e)}>
             <input type="file" accept="image/*" />
             <button type="submit">submit</button>
           </form>
@@ -113,6 +132,11 @@ export const Profile = () => {
         )}
         {showChangePasswordFields && (
           <form onSubmit={(e) => handleSubmit(e)}>
+            <input placeholder="Enter old password..."
+              value={oldPassword}
+              type="password"
+              onChange={(e) => setOldPassword(e.target.value)}
+            ></input>
             <input
               placeholder="Enter new password..."
               value={newPassword}
